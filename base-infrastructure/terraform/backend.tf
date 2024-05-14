b388496cab83289d9483a44cee2337daed80331a
Tarashish Mishra <sunu@users.noreply.github.com>
2024-05-14 16:25:43 +0530

    update storage account name

--- base-infrastructure/terraform/backend.tf
+++ base-infrastructure/terraform/backend.tf
@@ -7,7 +7,7 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "ifrctgos002rg"
-    storage_account_name = "ifrcgoterraform"
+    storage_account_name = "gosandbox1"
     container_name       = "terraform"
     key                  = "playground"
   }
