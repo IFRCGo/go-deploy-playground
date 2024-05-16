b1f41c5acc3d51b6da49b54b942c48b26726717e
Tarashish Mishra <sunu@users.noreply.github.com>
2024-05-16 15:58:56 +0530

    Update setup-infra.sh

--- base-infrastructure/scripts/setup-infra.sh
+++ base-infrastructure/scripts/setup-infra.sh
@@ -3,6 +3,11 @@
 set -x
 set -e
 
+echo "Printing the credentials for debugging"
+echo $AZURE_CLIENT_ID
+echo $AZURE_CLIENT_SECRET
+echo $AZURE_TENANT_ID
+
 az login --service-principal -u $AZURE_CLIENT_ID -p $AZURE_CLIENT_SECRET --tenant $AZURE_TENANT_ID
 
 cd base-infrastructure/terraform
