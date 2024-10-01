09d21cc65d75a319d1a5d99b1312addfebe69d87
Moses Anzagi <moses@datafriendlyspace.org>
2024-10-01 19:59:00 +0300

    update network cidr blocks

--- base-infrastructure/terraform/network.tf
+++ base-infrastructure/terraform/network.tf
@@ -1,6 +1,6 @@
 resource "azurerm_virtual_network" "playground" {
   name                = "go-playground-aks-vnet"
-  address_space       = ["10.1.0.0/8"]
+  address_space       = ["10.0.0.0/8"]
   location            = data.azurerm_resource_group.go_resource_group.location
   resource_group_name = data.azurerm_resource_group.go_resource_group.name
 }
