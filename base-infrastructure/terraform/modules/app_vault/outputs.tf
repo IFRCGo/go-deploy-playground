a89daf79ac3b59cf09ee2a7ff2fb544eff9682f2
Moses Anzagi <moses@datafriendlyspace.org>
2024-10-01 09:49:00 +0300

    rename outputs of vault module

--- base-infrastructure/terraform/modules/app_vault/outputs.tf
+++ base-infrastructure/terraform/modules/app_vault/outputs.tf
@@ -2,6 +2,10 @@ output "key_vault_name" {
   value = azurerm_key_vault.app_kv.name
 }
 
-output "workload_id" {
+output "workload_client_id" {
   value = azurerm_user_assigned_identity.workload.client_id
+}
+
+output "workload_id"{
+  value = azurerm_user_assigned_identity.workload.id
 }
