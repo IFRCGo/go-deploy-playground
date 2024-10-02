a6cf7b650adac50cabac2ab33dc73064d3813144
Moses Anzagi <moses@datafriendlyspace.org>
2024-10-02 09:24:44 +0300

    disable helm installations

--- base-infrastructure/terraform/helm.tf
+++ base-infrastructure/terraform/helm.tf
@@ -1,34 +1,34 @@
-provider "helm" {
-  kubernetes {
-    host                   = azurerm_kubernetes_cluster.go_kubernetes_cluster.kube_config.0.host
-    client_certificate     = base64decode(azurerm_kubernetes_cluster.go_kubernetes_cluster.kube_config.0.client_certificate)
-    client_key             = base64decode(azurerm_kubernetes_cluster.go_kubernetes_cluster.kube_config.0.client_key)
-    cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.go_kubernetes_cluster.kube_config.0.cluster_ca_certificate)
-  }
-}
-
-resource "helm_release" "go-ingress-nginx" {
-  name             = "ingress-nginx"
-  repository       = "https://kubernetes.github.io/ingress-nginx"
-  chart            = "ingress-nginx"
-  version          = "4.8.3"
-  namespace        = "ingress-nginx"
-  create_namespace = true
-}
-
-resource "helm_release" "go-cert-manager" {
-  name             = "cert-manager"
-  repository       = "https://charts.jetstack.io"
-  chart            = "cert-manager"
-  version          = "v1.13.2"
-  namespace        = "cert-manager"
-  create_namespace = true
-
-  set {
-    name  = "installCRDs"
-    value = true
-  }
-}
+#provider "helm" {
+#  kubernetes {
+#    host                   = azurerm_kubernetes_cluster.go_kubernetes_cluster.kube_config.0.host
+#    client_certificate     = base64decode(azurerm_kubernetes_cluster.go_kubernetes_cluster.kube_config.0.client_certificate)
+#    client_key             = base64decode(azurerm_kubernetes_cluster.go_kubernetes_cluster.kube_config.0.client_key)
+#    cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.go_kubernetes_cluster.kube_config.0.cluster_ca_certificate)
+#  }
+#}
+#
+#resource "helm_release" "go-ingress-nginx" {
+#  name             = "ingress-nginx"
+#  repository       = "https://kubernetes.github.io/ingress-nginx"
+#  chart            = "ingress-nginx"
+#  version          = "4.8.3"
+#  namespace        = "ingress-nginx"
+#  create_namespace = true
+#}
+#
+#resource "helm_release" "go-cert-manager" {
+#  name             = "cert-manager"
+#  repository       = "https://charts.jetstack.io"
+#  chart            = "cert-manager"
+#  version          = "v1.13.2"
+#  namespace        = "cert-manager"
+#  create_namespace = true
+#
+#  set {
+#    name  = "installCRDs"
+#    value = true
+#  }
+#}
 #
 #resource "helm_release" "argo-cd" {
 #  name             = "argo-cd"
