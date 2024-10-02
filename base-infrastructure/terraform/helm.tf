78ad5e674da60362d136c970a67e1572021cf0ea
Moses Anzagi <moses@datafriendlyspace.org>
2024-10-02 18:28:11 +0300

    uninstall helm releases

--- base-infrastructure/terraform/helm.tf
+++ base-infrastructure/terraform/helm.tf
@@ -7,28 +7,28 @@ provider "helm" {
   }
 }
 
-resource "helm_release" "go-ingress-nginx" {
-  name             = "ingress-nginx"
-  repository       = "https://kubernetes.github.io/ingress-nginx"
-  chart            = "ingress-nginx"
-  version          = "4.8.3"
-  namespace        = "ingress-nginx"
-  create_namespace = true
-}
-
-resource "helm_release" "go-cert-manager" {
-  name             = "cert-manager"
-  repository       = "https://charts.jetstack.io"
-  chart            = "cert-manager"
-  version          = "v1.13.2"
-  namespace        = "cert-manager"
-  create_namespace = true
-
-  set {
-    name  = "installCRDs"
-    value = true
-  }
-}
+#resource "helm_release" "go-ingress-nginx" {
+#  name             = "ingress-nginx"
+#  repository       = "https://kubernetes.github.io/ingress-nginx"
+#  chart            = "ingress-nginx"
+#  version          = "4.8.3"
+#  namespace        = "ingress-nginx"
+#  create_namespace = true
+#}
+#
+#resource "helm_release" "go-cert-manager" {
+#  name             = "cert-manager"
+#  repository       = "https://charts.jetstack.io"
+#  chart            = "cert-manager"
+#  version          = "v1.13.2"
+#  namespace        = "cert-manager"
+#  create_namespace = true
+#
+#  set {
+#    name  = "installCRDs"
+#    value = true
+#  }
+#}
 #
 #resource "helm_release" "argo-cd" {
 #  name             = "argo-cd"
