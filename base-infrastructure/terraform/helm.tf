bf9585ad48c0cf4d734eafe3b5c6032060f9a2e0
Moses Anzagi <moses@datafriendlyspace.org>
2025-01-28 08:49:57 +0300

    fix error in helm private repo spec

--- base-infrastructure/terraform/helm.tf
+++ base-infrastructure/terraform/helm.tf
@@ -36,7 +36,7 @@ resource "helm_release" "argo-cd" {
             "url" : "https://${module.go_container_registry.registry_server}/ifrcgo/sdt-api"
             "username" : "${module.go_container_registry.acr_token_username}"
             "password" : "${module.go_container_registry.acr_token_password}"
-            "enableOCI" : true
+            "enableOCI" : "true"
           }
         ]
       }
