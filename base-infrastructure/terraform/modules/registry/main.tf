9d23a1388d1addcf9e4a4fac68cb459526425c17
Moses Anzagi <moses@datafriendlyspace.org>
2025-02-06 13:56:54 +0300

    use Standard sku for registry

--- base-infrastructure/terraform/modules/registry/main.tf
+++ base-infrastructure/terraform/modules/registry/main.tf
@@ -2,5 +2,6 @@ resource "azurerm_container_registry" "shared" {
   name                = "${title(var.app_name)}${title(var.environment)}ContainerRegistry"
   resource_group_name = data.azurerm_resource_group.app_rg.name
   location            = data.azurerm_resource_group.app_rg.location
-  sku                 = "Basic"
+  #sku                 = "Basic"
+  sku                 = "Standard"
 }
