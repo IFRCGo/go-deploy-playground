eff5198ec618240f259b893070ba8c2fe5d0208d
Moses Anzagi <moses@datafriendlyspace.org>
2025-02-06 13:35:46 +0300

    add custom AcrToken permissions for CI principal

--- base-infrastructure/terraform/modules/registry/ci_access.tf
+++ base-infrastructure/terraform/modules/registry/ci_access.tf
@@ -26,3 +26,28 @@ resource "azurerm_container_registry_token_password" "ci" {
   password1 {
   }
 }
+
+# Create a Custom Role for Accessing Token Passwords
+resource "azurerm_role_definition" "acr_token_password_reader" {
+  name        = "ACR Token Password Reader"
+  scope       = azurerm_container_registry.shared.id
+  description = "Custom role to allow reading ACR token passwords."
+
+  permissions {
+    actions = [
+      "Microsoft.ContainerRegistry/registries/tokens/read",
+      "Microsoft.ContainerRegistry/registries/credentials/list/action",
+    ]
+    not_actions = []
+  }
+
+  assignable_scopes = [
+    azurerm_container_registry.shared.id
+  ]
+}
+
+resource "azurerm_role_assignment" "acr_token_password_access" {
+  scope              = azurerm_container_registry.shared.id
+  role_definition_id = azurerm_role_definition.acr_token_password_reader.role_definition_resource_id
+  principal_id       = data.azurerm_client_config.current.object_id
+}
--- base-infrastructure/terraform/modules/registry/data.tf
+++ base-infrastructure/terraform/modules/registry/data.tf
@@ -1,3 +1,6 @@
 data "azurerm_resource_group" "app_rg" {
   name = var.resource_group_name
 }
+
+data "azurerm_client_config" "current" {
+}
