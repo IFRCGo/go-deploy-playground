103c481d1f8c131d679aac1d629e55ebb928cd30
Moses Anzagi <moses@datafriendlyspace.org>
2025-02-23 17:19:56 +0300

    remove alert-hub related resources

--- base-infrastructure/terraform/outputs.tf
+++ base-infrastructure/terraform/outputs.tf
@@ -10,17 +10,17 @@ output "risk_module_client_id" {
   value = module.secrets.workload_client_id
 }
 
-output "alert_hub_key_vault_name" {
-  value = module.alert_hub_vault.key_vault_name
-}
-
-output "alert_hub_client_id" {
-  value = module.alert_hub_vault.workload_client_id
-}
-
-output "alert_hub_storage_container" {
-  value = module.alert_hub_vault.storage_container_names
-}
+#output "alert_hub_key_vault_name" {
+#  value = module.alert_hub_vault.key_vault_name
+#}
+#
+#output "alert_hub_client_id" {
+#  value = module.alert_hub_vault.workload_client_id
+#}
+#
+#output "alert_hub_storage_container" {
+#  value = module.alert_hub_vault.storage_container_names
+#}
 
 # Montandon ETL
 output "montandon_key_vault_name" {
--- base-infrastructure/terraform/vaults_setup.tf
+++ base-infrastructure/terraform/vaults_setup.tf
@@ -24,36 +24,36 @@ module "secrets" {
   vault_subnet_ids = [azurerm_subnet.app.id]
 }
 
-module "alert_hub_vault" {
-  source = "./modules/app_vault"
-
-  app_name                = "alert-hub"
-  cluster_namespace       = "alert-hub"
-  cluster_oidc_issuer_url = azurerm_kubernetes_cluster.go_kubernetes_cluster.oidc_issuer_url
-  database_server_id      = azurerm_postgresql_flexible_server.ifrc.id
-  environment             = var.environment
-  resource_group_name     = data.azurerm_resource_group.go_resource_group.name
-  service_account_name    = "alert-hub-sa"
-
-  secrets = {
-    DB_USER     = var.psql_administrator_login
-    DB_PASSWORD = random_password.db_admin.result
-    DB_HOST     = azurerm_postgresql_flexible_server.ifrc.fqdn
-  }
-
-  storage_config = {
-    container_refs = [
-      "media",
-      "static"
-    ]
-
-    enabled              = true
-    storage_account_id   = azurerm_storage_account.app_storage.id
-    storage_account_name = azurerm_storage_account.app_storage.name
-  }
-
-  vault_subnet_ids = [azurerm_subnet.app.id]
-}
+#module "alert_hub_vault" {
+#  source = "./modules/app_vault"
+#
+#  app_name                = "alert-hub"
+#  cluster_namespace       = "alert-hub"
+#  cluster_oidc_issuer_url = azurerm_kubernetes_cluster.go_kubernetes_cluster.oidc_issuer_url
+#  database_server_id      = azurerm_postgresql_flexible_server.ifrc.id
+#  environment             = var.environment
+#  resource_group_name     = data.azurerm_resource_group.go_resource_group.name
+#  service_account_name    = "alert-hub-sa"
+#
+#  secrets = {
+#    DB_USER     = var.psql_administrator_login
+#    DB_PASSWORD = random_password.db_admin.result
+#    DB_HOST     = azurerm_postgresql_flexible_server.ifrc.fqdn
+#  }
+#
+#  storage_config = {
+#    container_refs = [
+#      "media",
+#      "static"
+#    ]
+#
+#    enabled              = true
+#    storage_account_id   = azurerm_storage_account.app_storage.id
+#    storage_account_name = azurerm_storage_account.app_storage.name
+#  }
+#
+#  vault_subnet_ids = [azurerm_subnet.app.id]
+#}
 
 module "sdt_vault" {
   source = "./modules/app_vault"
